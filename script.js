56d837e57ea8370210020cb403312c8f1f509eae
travelways000-max <travelways000@gmail.com>
2026-03-30 17:26:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,36 @@ function type() {
 }
 
 type();
+const textElement = document.getElementById("typing");
+const phrases = [
+  "Future Cybersecurity Expert...",
+  "Music Producer & Creator...",
+  "AI & Automation Enthusiast...",
+  "Computer Engineering Student."
+];
+
+let phraseIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < phrases[phraseIndex].length) {
+    textElement.textContent += phrases[phraseIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, 100);
+  } else {
+    setTimeout(erase, 2000);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    textElement.textContent = phrases[phraseIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, 50);
+  } else {
+    phraseIndex = (phraseIndex + 1) % phrases.length;
+    setTimeout(type, 500);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", type);
